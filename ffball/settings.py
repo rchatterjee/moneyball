a05864b18c0f65892e6c05b50801a9d0bf4990e7
Rahul Chatterjee <rchatterjee4@wisc.edu>
2014-03-16 17:04:46 -0500

    admin database added

--- ffball/settings.py
+++ ffball/settings.py
@@ -57,7 +57,8 @@ WSGI_APPLICATION = 'ffball.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME'  : os.path.join(BASE_DIR, 'db.sqlite3')
         }
     }
 # Internationalization
