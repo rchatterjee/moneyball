f4bf972a5e57c39e9b4f48a7591668d9362cf800
Shoban Preeth <shoban.preeth@gmail.com>
2014-03-27 01:23:38 -0500

    Proteam API done.
    Example:
    /data/proteam/ -- Lists all teams.
    /data/proteam/ARI -- Returns team info of 'ARI' (Arizona)
    /data/proteam/ARI/players -- Returns player list of 'ARI' (Arizona)

--- data/urls.py
+++ data/urls.py
@@ -5,8 +5,8 @@ from data.views import proplayer as pviews
 
 urlpatterns = patterns('',
     url(r'^proteam/$', tviews.index, name='proteam.index'),
-    url(r'^proteam/(?P<team_id>\d+)$', tviews.info, name='proteam.info'),
-    url(r'^proteam/(?P<team_id>\d+)/players$',
+    url(r'^proteam/(?P<team_id>\w+)$', tviews.info, name='proteam.info'),
+    url(r'^proteam/(?P<team_id>\w+)/players$',
         tviews.players, name='proteam.players'),
     url(r'^proplayer/$', pviews.index, name='index'),
     url(r'^proplayer/(?P<player_id>\d+)/$', pviews.info, name='index'),
