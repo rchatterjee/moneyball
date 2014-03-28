209c37896d1d980237be7f4a0ebe161b95928971
Saikat R. Gomes <saikatgomes@gmail.com>
2014-03-28 05:13:22 -0500

    added user info in context as me

--- draft/views.py
+++ draft/views.py
@@ -24,24 +24,26 @@ def mockdraft(request):
     draftList = League.objects.filter(vendor__name = 'moneyball')
     draftList = draftList.annotate(teamCount=Count('team'))
     draftList = draftList.order_by('settings__draft_date','teamCount')
-    for i in range(len(draftList)):
-        if draftList[i].password:
-            draftList[i].password = '*'
-        else : draftList[i].password = ''
     context = app.helpers.user_template_dict(request)
+    if context["logged_in"]:
+        myTeamList = Team.objects.filter(user = request.user).only('league')
+        context['myTeamList'] = myTeamList
     context['next_page'] = request.get_full_path
     context['providers'] = ['facebook', 'yahoo', 'google', 'github']
     context['draftList'] = draftList
+    context['me'] = request.user
     return render(request, 'mock.html', context)
 
 
 def draftroom(request, draftroom_id):
     context = app.helpers.user_template_dict(request)
     context['next_page'] = request.get_full_path
+    context['me'] = request.user
     return render(request, 'draftroom.html', context)
 
 def draft(request, draft_id=0):
     context = app.helpers.user_template_dict(request)
     context['next_page'] = request.get_full_path
+    context['me'] = request.user
     return render(request, 'draft.html', context)
 
