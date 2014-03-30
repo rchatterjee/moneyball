d9f2794d6c47c0557cfc2a0262e172cd77b8e53d
Shoban Preeth <shoban.preeth@gmail.com>
2014-03-30 15:02:12 -0500

    Modify create-db.sh script for 'south'.

--- db/create-db.sh
+++ db/create-db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -x
-# rm -rf db.sqlite3
+rm -rf db.sqlite3
 (cd ..; python manage.py syncdb --noinput)
+(cd ..; python manage.py migrate)
 ./dump2sqlite.sh mysql_sqlite3.sql | sqlite3 db.sqlite3
